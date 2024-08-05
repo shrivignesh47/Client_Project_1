a99f11c3967f3c569cad81ff9fd3daf02b93dba5
shrivignesh37 <shrivigneshsumathi@gmail.com>
2024-08-05 22:46:08 +0530

    Initial version of crm generated by generator-jhipster@8.6.0

---
+++ Desktop/crm-95c64737cb7975211c62b91bf1a3239568dc6873/crm/src/main/java/com/mycompany/myapp/domain/enumeration/acc.java
@@ -0,0 +1,20 @@
+package com.mycompany.myapp.domain.enumeration;
+
+/**
+ * The acc_type enumeration.
+ */
+public enum acc_type {
+    NONE,
+    ANALYST,
+    COMPETITOR,
+    CUSTOMER,
+    DISTRIBUTOR,
+    INTEGRATOR,
+    INVESTER,
+    OTHER,
+    PARTNER,
+    PRESS,
+    RESELLER,
+    SUPPLIER,
+    VENDOR,
+}
